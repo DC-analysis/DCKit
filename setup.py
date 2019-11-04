64f1b26b5c7bbe6955456f268b7d94813b8f78e8
Paul Müller <dev@craban.de>
2019-11-04 17:52:28 +0100

    ci: another try

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     description=description,
     long_description=open('README.rst').read() if exists('README.rst') else '',
     install_requires=[# pin DCKit to a specific version of Shape-Out and dclab
-                      "shapeout>0.9.6,<0.9.8",
+                      "shapeout==0.9.6.post4",
                       "dclab[all]==0.16.1",
                       "h5py>=2.8.0",
                       "pyqt5",
