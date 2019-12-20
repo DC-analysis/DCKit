72e1d7d6038c07e36851122bbe2c23272aa11f12
Paul Müller <dev@craban.de>
2019-12-20 17:32:25 +0100

    setup: bump shapeout from 0.9.6.post6 to 0.9.8

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     description=description,
     long_description=open('README.rst').read() if exists('README.rst') else '',
     install_requires=[# pin DCKit to a specific version of Shape-Out and dclab
-                      "shapeout==0.9.6.post6",
+                      "shapeout==0.9.8",
                       "dclab[all]==0.20.0",
                       "h5py>=2.8.0",
                       "pyqt5",
