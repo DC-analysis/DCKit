ee44fea11d74efe324f1f98b29490b37fc56523a
Paul Müller <dev@craban.de>
2022-06-10 13:36:53 +0200

    setup: bump dclab from 0.40.0 to 0.42.2

--- build-recipes/hook-dckit.py
+++ build-recipes/hook-dckit.py
@@ -7,11 +7,12 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 # -----------------------------------------------------------------------------
 
-from PyInstaller.utils.hooks import collect_data_files
+from PyInstaller.utils.hooks import collect_data_files, collect_submodules
 
 # Data files
 datas = collect_data_files("dckit", include_py_files=True)
 datas += collect_data_files("dckit", subdir="img")
 
 # This is excluded by the PIL-hook and not included automatically
-hiddenimports = ["PyQt5", "imageio_ffmpeg"]
+hiddenimports = ["PyQt5"]
+
