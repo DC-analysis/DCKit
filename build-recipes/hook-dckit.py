123cb1ac05ed09a48c921c9eb0bdb37e6be3454f
Paul Müller <dev@craban.de>
2022-06-10 13:57:52 +0200

    flake8

--- build-recipes/hook-dckit.py
+++ build-recipes/hook-dckit.py
@@ -7,7 +7,7 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 # -----------------------------------------------------------------------------
 
-from PyInstaller.utils.hooks import collect_data_files, collect_submodules
+from PyInstaller.utils.hooks import collect_data_files
 
 # Data files
 datas = collect_data_files("dckit", include_py_files=True)
@@ -15,4 +15,3 @@ datas += collect_data_files("dckit", subdir="img")
 
 # This is excluded by the PIL-hook and not included automatically
 hiddenimports = ["PyQt5"]
-
