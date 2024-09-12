1ab6343609bee67801e609e0f7452e0a19968546
Paul Müller <dev@craban.de>
2024-09-12 10:54:44 +0200

    setup: bump dclab from 0.60.0 to 0.61.1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     license="GPL v3",
     description=description,
     long_description=open('README.rst').read() if exists('README.rst') else '',
-    install_requires=["dclab[tdms]>=0.60.0",
+    install_requires=["dclab[tdms]>=0.61.1",
                       "h5py>=3.9.0",
                       # https://github.com/imageio/imageio/issues/766
                       "imageio[ffmpeg]==2.10.3",
